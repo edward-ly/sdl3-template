14ad86bc1724a9f7267e98aadef934af83565c1e
Edward Ly <contact@edward.ly>
2025-07-10 17:47:16 -0700

    chore(android): update project from .env values
    Signed-off-by: Edward Ly <contact@edward.ly>

--- android-project/app/src/main/java/org/libsdl/helloworld/HelloWorldActivity.java
+++ android-project/app/src/main/java/org/libsdl/helloworld/HelloWorldActivity.java
@@ -3,7 +3,7 @@ package org.libsdl.helloworld;
 import org.libsdl.app.SDLActivity;
 
 public class HelloWorldActivity extends SDLActivity {
-    protected String[] getLibraries() {
-        return new String[] { "SDL3", "sdl-helloworld" };
-    }
+	protected String[] getLibraries() {
+		return new String[] { "SDL3", "sdl-helloworld" };
+	}
 }
